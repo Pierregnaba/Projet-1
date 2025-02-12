c6820e46133189056ebeb8a9cbd1463008c9cc90
Pierregnaba <151533040+Pierregnaba@users.noreply.github.com>
2025-02-12 12:38:42 +0000

    Delete TEST SIMPLON directory

--- TEST SIMPLON/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("link").addEventListener("click", function (event) {
-  event.preventDefault();
-  const confirmation = window.confirm(
-    "Vous allez être redirigé vers un autre site. Voulez-vous continuer ?"
-  );
-  if (confirmation) {
-    window.location.href = this.href;
-  }
-});
